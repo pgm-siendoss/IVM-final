1abbb06127b0b64b3bc717c31ad811b595fd64b7
pgm-siendoss <sienna.dossche@student.arteveldehs.be>
2022-06-03 09:16:03 +0200

    Problem w const IdleTimer

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,10 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useRouter } from 'next/router';
 
 import Nav from "../components/Nav";
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import gsap from "gsap";
 import { lightTheme } from "../constants/styles";
+
+export const IdleTimer = () => {
+
+  const router = useRouter();
+  let timeout = null;
+
+  const goBackToHome = () => {
+     // reset the application here
+  };
+
+  const restartAutoReset = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      goBackToHome();
+    }, 1000 * 30);
+  };
+
+  const onMouseMove = () => {
+    restartAutoReset();
+  };
+
+  useEffect(() => {
+    restartAutoReset();
+    window.addEventListener('mousemove', _onMouseMove);
+
+    // cleanup function
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+        window.removeEventListener('mousemove', onMouseMove);
+      }
+    };
+  }, [router.pathname]);
+
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
 
 const BaseLayout = () => {
   const [time, setTime] = useState(0);
